f8c45e239446d0a820f5cdf43596a13415455d99
Arindam <arindamkeswani@gmail.com>
2022-03-21 14:35:45 +0530

    Dockerized the application

--- sample/sample/urls.py
+++ sample/sample/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('app/', include('sampleApp.urls')),
+    path('', include('sampleApp.urls')),
     path('admin/', admin.site.urls),
 ]
